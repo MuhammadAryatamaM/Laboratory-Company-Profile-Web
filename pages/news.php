db66fdc64123aa49201ccdfc3134bd388f2d960a
angel chelssa <angelchelsa150526@gmail.com>
2025-11-28 22:26:34 +0700

    update news.php

--- pages/news.php
+++ pages/news.php
@@ -15,7 +15,7 @@
 // Data Berita Utama
 $mainNews = [
     'title' => 'ICCE 2023, Full Paper Presentation',
-    'image' => '../assets/img/gallery2.png',
+    'image' => '../assets/img/news/gallery7.png',
     'description' => 'Kegiatan pemaparan makalah di ICCE 2023 di Matsue, Jepang merupakan ajang ilmiah internasional yang mempertemukan peneliti, pengajar, dan mahasiswa untuk mempresentasikan hasil penelitian di bidang teknologi pendidikan. Peserta yang lolos seleksi menyampaikan temuan mereka di hadapan para ahli, disertai sesi tanya jawab untuk memperoleh masukan. Melalui kegiatan ini, peneliti dapat memperkenalkan karya mereka secara global, membangun kolaborasi, dan meningkatkan kualitas penelitian di masa depan.'
 ];
 
@@ -23,40 +23,59 @@ $mainNews = [
 $recentItems = [
     [
         'title' => 'Visiting Scientist Program',
-        'image' => '../assets/img/gallery2.png',
+        'image' => '../assets/img/news/gallery8.png',
         'location' => 'Japan',
         'date' => 'November 2023'
     ],
     [
         'title' => 'Monthly Research Discussion',
-        'image' => '../assets/img/gallery2.png',
-        'location' => 'Politeknik',
+        'image' => '../assets/img/news/gallery9.png',
+        'location' => 'Polinema',
         'date' => 'Januari 2024'
     ],
     [
         'title' => 'International Research Discussion Program',
-        'image' => '../assets/img/gallery2.png',
+        'image' => '../assets/img/news/gallery10.png',
         'location' => 'Japan',
         'date' => 'Desember 2023'
     ],
     [
-        'title' => 'Workshop AI in Education',
-        'image' => '../assets/img/gallery2.png',
-        'location' => 'Jakarta',
-        'date' => 'Februari 2024'
+        'title' => 'ICCE 2023, Full Paper Presentation',
+        'image' => '../assets/img/news/gallery6.png',
+        'location' => 'Japan',
+        'date' => 'November 2023'
     ],
     [
-        'title' => 'Seminar Machine Learning',
-        'image' => '../assets/img/gallery2.png',
-        'location' => 'Surabaya',
-        'date' => 'Maret 2024'
+        'title' => 'Best Overall Paper Award',
+        'image' => '../assets/img/news/gallery5.png',
+        'location' => 'Jaoan',
+        'date' => 'November 2023'
     ],
     [
-        'title' => 'Conference Data Science',
-        'image' => '../assets/img/gallery2.png',
-        'location' => 'Bandung',
-        'date' => 'April 2024'
+        'title' => 'POLINEMA Research EXPO 2024',
+        'image' => '../assets/img/news/gallery4.png',
+        'location' => 'Polinema',
+        'date' => 'November 2023'
+    ],
+    [
+        'title' => 'ICAST 2024 Bandung',
+        'image' => '../assets/img/news/gallery3.png',
+        'location' => 'Indonesia',
+        'date' => 'November 2023'
+    ],
+    [
+        'title' => 'ICCE 2024 Atteneo University',
+        'image' => '../assets/img/news/gallery2.png',
+        'location' => 'Phillipines',
+        'date' => 'November 2023'
+    ],
+    [
+        'title' => 'ECTEL 2024 Krems',
+        'image' => '../assets/img/news/gallery1.png',
+        'location' => 'Austria',
+        'date' => 'November 2023'
     ]
+
 ];
 
 // Fungsi untuk generate recent item
