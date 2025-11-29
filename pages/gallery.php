0706065c7503de06a5d61819f1e247aef4a8322d
angel chelssa <angelchelsa150526@gmail.com>
2025-11-29 13:55:12 +0700

    update gallery.php

--- pages/gallery.php
+++ pages/gallery.php
@@ -1,7 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<body>
-    <?php include '../layouts/header.php'; ?>
     <link rel="stylesheet" href="<?php echo $root; ?>assets/css/header.css">
     <link rel="stylesheet" href="../assets/css/gallery.css">
 
@@ -13,20 +11,20 @@
                 <?php
                 // Array gambar gallery
                 $gallery = [
-                    '../assets/img/gallery1.png',
-                    '../assets/img/gallery2.png',
-                    '../assets/img/gallery3.png',
-                    '../assets/img/gallery4.png',
-                    '../assets/img/gallery5.png',
-                    '../assets/img/gallery6.png',
-                    '../assets/img/gallery7.png',
-                    '../assets/img/gallery8.png',
-                    '../assets/img/gallery9.png',
-                    '../assets/img/gallery10.png',
-                    '../assets/img/gallery11.png',
-                    '../assets/img/gallery12.png',
-                    '../assets/img/gallery13.png',
-                    '../assets/img/gallery14.png'
+                    'assets/img/gallery1.png',
+                    'assets/img/gallery2.png',
+                    'assets/img/gallery3.png',
+                    'assets/img/gallery4.png',
+                    'assets/img/gallery5.png',
+                    'assets/img/gallery6.png',
+                    'assets/img/gallery7.png',
+                    'assets/img/gallery8.png',
+                    'assets/img/gallery9.png',
+                    'assets/img/gallery10.png',
+                    'assets/img/gallery11.png',
+                    'assets/img/gallery12.png',
+                    'assets/img/gallery13.png',
+                    'assets/img/gallery14.png'
                 ];
 
                 // Loop untuk menampilkan setiap gambar
@@ -40,7 +38,6 @@
         </div>
     </section>
 
-    <?php include '../layouts/footer.php'; ?>
     <link rel="stylesheet" href="<?php echo $root; ?>assets/css/footer.css">
 
     <script src="../assets/js/script.js"></script>
